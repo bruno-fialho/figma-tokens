b910a6fe5946e0bf47790fb2587862fbdf19e408
Bruno Fialho <brugerf@gmail.com>
2024-04-06 12:17:18 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 06 Apr 2024 14:14:56 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c5",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8257e6"
+};
